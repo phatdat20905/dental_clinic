4046137b75f4b5d036ce1da6cf19874d0e630e79
phatdat20905 <datnp20905@gmail.com>
2024-12-09 20:26:52 +0700

    ket noi database sql server thanh cong

--- dental_clinic/dental_clinic/settings.py
+++ dental_clinic/dental_clinic/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'dental_clinic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "mssql",
+        "NAME": "dental_clinic",
+        "USER": "SA",
+        "PASSWORD": "YourPassword123!",
+        "HOST": "localhost",
+        "PORT": "1444",
+        "OPTIONS": {
+            "driver": "ODBC Driver 17 for SQL Server", 
+            "trusted_connection": "yes",
+        }
     }
 }
 
